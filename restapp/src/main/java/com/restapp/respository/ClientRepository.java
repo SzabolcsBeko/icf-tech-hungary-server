f2674ffd7d843e2ea07c251082f969000a246b95
abc <abc@example.com>
2020-08-02 23:37:12 +0200

    Fourth commit, Amending repository methods

--- restapp/src/main/java/com/restapp/respository/ClientRepository.java
+++ restapp/src/main/java/com/restapp/respository/ClientRepository.java
@@ -18,14 +18,8 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
 	@Query(value = "SELECT * FROM Client c WHERE c.id = :id", nativeQuery = true)
 	Client findClientById(@Param("id") Long id);
 	
-	/*@Query(value = "SELECT * FROM CLIENT WHERE ID = ?1", nativeQuery = true)
-	Client findClientById(String id);*/
-	
 	@Query(value = "SELECT * FROM Client c WHERE c.name = :name AND c.password = :password", nativeQuery = true)
 	List<Client> findClinetByNameAndPassword(@Param("name") String name, @Param("password") String password);
-	
-	/*@Query(value = "SELECT * FROM CLIENT WHERE NAME=?1 AND PASWORD=?2", nativeQuery = true)
-	Client findClientByNameAndPassword(String name, String password);*/
 	
 	@Query(value = "SELECT * FROM Client ORDER BY id", nativeQuery = true)
 	List<Client> findAllClients(String id);
@@ -33,7 +27,7 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
 	@Modifying
 	@Query(
 	  value = 
-	    "INSERT INTO Client (name, passwrd, lastLogin, roles) VALUES (:name, :passwrd, :lastLogin, :roles)", nativeQuery = true)
+	    "INSERT INTO Client (name, password, lastLogin, roles) VALUES (:name, :passwrd, :lastLogin, :roles)", nativeQuery = true)
 	void insertClient(@Param("name") String name, @Param("passwrd") String passwrd, @Param("lastLogin") String lastLogin, @Param("roles") String roles);
 	
 
